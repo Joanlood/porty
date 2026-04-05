d8b514d154e6e893bff36829ee243702fa6e7e85
Tokyo187 <joanlood2@gmail.com>
2026-04-05 16:21:36 +0200

    Fix installation check for porty script

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ clear
 SCRIPT_URL="https://raw.githubusercontent.com/Joanlood/porty/refs/heads/main/porty.sh"
 INSTALL_PATH="/usr/local/bin/porty"
 
-if [ -f "/usr/local/bin/porty.sh" ]; then
+if [ -f "/usr/local/bin/porty" ]; then
     echo "⚠️ Porty is already installed! Run with: porty"
     exit 0
 fi
